c1a2ee8ce3db46b5b7aa83f57b482b16c9383739
Hosana Azevedo <hosanapires9@gmail.com>
2026-04-21 15:03:20 -0300

    adiciona exercício 1005

---
+++ java/1005.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        double a, b, media;
+
+        Scanner teclado = new Scanner(System.in);
+        a = teclado.nextDouble();
+        b = teclado.nextDouble();
+        media = ((a * 3.5) + (b * 7.5)) / 11;
+
+        System.out.printf("MEDIA = %.5f\n", media);
+        teclado.close();
+
+    }
+
+}
